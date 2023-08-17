463c867d466f1896b2a2b23f73518c6c247f0f37
abadnjers <gabadnjers@fcien.edu.uy>
2023-08-17 15:59:35 -0300

    Commit para que me deje pullear. no hacer merge!!

---
+++ js/products.js
@@ -0,0 +1,28 @@
+function fetchData(url) {
+    return fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error al obtener los datos");
+        }
+      })
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error("Error al cargar el archivo JSON:", error);
+      });
+  }
+
+
+document.getElementById("Carlos").addEventListener("click", async function () {
+    try {
+      const productosJson = await fetchData("https://japceibal.github.io/emercado-api/cats_products/101.json"); // Espera a que se resuelva la promesa
+      console.log(" PRODUCTOS:");
+      console.log(productosJson.products[0
+    ]);
+    } catch (error) {
+        console.error("Error:", error);
+      }
+    });
