df56285e412387a5d146cae859dad14094a4b197
Joaquin Perez <joaquin.perez.coria@gmail.com>
2023-11-10 15:40:45 -0300

    Arreglado los inputs del perfil autocompletados

--- js/my-profile.js
+++ js/my-profile.js
@@ -6,7 +6,7 @@ const primerNombre = document.getElementById("primerNombre");
 const segundoNombre = document.getElementById("segundoNombre");
 const primerApellido = document.getElementById("primerApellido");
 const segundoApellido = document.getElementById("segundoApellido");
-const userMail = document.getElementById("userEmail");
+const userMail = document.getElementById("userMail");
 const avatarImg = document.getElementById("avatarImg");
 const userTelefono = document.getElementById("userTelefono");
 const userPais = document.getElementById("userPais");
@@ -15,22 +15,26 @@ const botonGuardar = document.getElementById("botonGuardar");
 
 document.addEventListener("DOMContentLoaded", async () => {
   // PUNTO 3 DEL ENTREGABLE:
-  primerNombre.value = user.primerNombre
-    ? user.primerNombre
-    : primerNombre.value;
-  segundoNombre.value = user.segundoNombre
-    ? user.segundoNombre
-    : segundoNombre.value;
-  primerApellido.value = user.primerApellido
-    ? user.primerApellido
-    : primerApellido.value;
-  segundoApellido.value = user.segundoApellido.value
-    ? user.segundoApellido
-    : segundoApellido.value;
-  userMail.value = user.mail ? user.mail : userMail.value;
-  userTelefono.value = user.telefono ? user.telefono : userTelefono.value;
-  userPais.value = user.pais ? user.pais : userPais.value;
-  userCiudad.value = user.ciudad ? user.ciudad : userCiudad.value;
+  if (user) {
+    primerNombre.value = user.primerNombre
+      ? user.primerNombre
+      : primerNombre.value;
+    segundoNombre.value = user.segundoNombre
+      ? user.segundoNombre
+      : segundoNombre.value;
+    primerApellido.value = user.primerApellido
+      ? user.primerApellido
+      : primerApellido.value;
+    segundoApellido.value = user.segundoApellido
+      ? user.segundoApellido
+      : segundoApellido.value;
+    userMail.value = user.mail ? user.mail : userMail.value;
+    console.log(userMail)
+    console.log(userTelefono)
+    userTelefono.value = user.telefono ? user.telefono : userTelefono.value;
+    userPais.value = user.pais ? user.pais : userPais.value;
+    userCiudad.value = user.ciudad ? user.ciudad : userCiudad.value;
+  }
 
   botonGuardar.addEventListener("click", async () => {
     const primerNombreValue = primerNombre.value;
